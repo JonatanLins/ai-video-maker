0957c9f439af74c54c673e4ea924b011b2681c46
Jonatan Lins <jonatanpinheiro99@gmail.com>
2019-03-12 22:16:04 -0300

    change: refactor index.js

--- index.js
+++ index.js
@@ -1,20 +1,19 @@
 const readline = require('readline-sync')
 
 function start () {
-  const content = {}
-
-  content.searchTerm = askAndReturnSearchTerm()
-  content.prefix = askAndReturnPrefix()
-
-  function askAndReturnSearchTerm() {
-    return readline.question('Type a search term: ')
-  }
-
-  function askAndReturnPrefix() {
+  const askAndReturnPrefix = () => {
     const prefixes = ['Who is', 'What is', 'The history of']
     const selectedPrefixIndex = readline.keyInSelect(prefixes, 'Choose one option')
     return prefixes[selectedPrefixIndex]
   }
+  
+  const askAndReturnSearchTerm = () => 
+    readline.question('Type a search term: ')
+
+  const content = {}
+
+  content.searchTerm = askAndReturnSearchTerm()
+  content.prefix = askAndReturnPrefix()
 
   console.log(content)
 }

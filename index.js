6134c992cf45a03ff4e57ebaf7711f397ea8ca58
Jonatan Lins <jonatanpinheiro99@gmail.com>
2019-03-12 22:10:27 -0300

    feat: add function to ask for prefix

--- index.js
+++ index.js
@@ -4,9 +4,16 @@ function start () {
   const content = {}
 
   content.searchTerm = askAndReturnSearchTerm()
+  content.prefix = askAndReturnPrefix()
 
   function askAndReturnSearchTerm() {
     return readline.question('Type a search term: ')
+  }
+
+  function askAndReturnPrefix() {
+    const prefixes = ['Who is', 'What is', 'The history of']
+    const selectedPrefixIndex = readline.keyInSelect(prefixes, 'Choose one option')
+    return prefixes[selectedPrefixIndex]
   }
 
   console.log(content)

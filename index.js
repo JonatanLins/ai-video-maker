1d954c5b0cf01f32e1a65b04aa43f8055d08a91a
Jonatan Lins <jonatanpinheiro99@gmail.com>
2019-03-12 22:42:21 -0300

    feat: add google trends option

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const readline = require('readline-sync')
+const RssParser = require('rss-parser')
 
-function start () {
+const trendsURL = 'https://trends.google.com/trends/trendingsearches/daily/rss?geo=BR'
+
+async function start () {
   function askAndReturnPrefix (searchTerm) {
     const prefixes = ['Who is', 'What is', 'The history of']
     const selectedPrefixIndex = readline
@@ -8,12 +11,28 @@ function start () {
     return prefixes[selectedPrefixIndex]
   }
   
-  const askAndReturnSearchTerm = () => 
-    readline.question('Type a search term: ')
+  async function askAndReturnSearchTerm () {
+    const response = readline
+      .question('Type a search term or G to fetch Google Trends: ')
+    return response.toUpperCase() === 'G' ? await askAndReturnTrend() : response
+  }
+  
+  async function askAndReturnTrend () {
+    console.log('Please wait...')
+    const trends = await getGoogleTrends()
+    const choice = readline.keyInSelect(trends, 'Choose a trend')
+    return trends[choice]
+  }
+
+  async function getGoogleTrends () {
+    const parser = new RssParser()
+    const trends = await parser.parseURL(trendsURL)
+    return trends.items.map(item => item.title)
+  }
 
   const content = {}
 
-  content.searchTerm = askAndReturnSearchTerm()
+  content.searchTerm = await askAndReturnSearchTerm()
   content.prefix = askAndReturnPrefix(content.searchTerm)
 
   console.log(content)
